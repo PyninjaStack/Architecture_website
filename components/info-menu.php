20135d4560594685f8d7b4208ef3b255c2e479ce
Syed Merazul Hassan <syedhassan1702@gmail.com>
2025-07-06 13:09:09 +0530

    remove comment in button of info-menu.php, little changes in one file only

--- components/info-menu.php
+++ components/info-menu.php
@@ -34,13 +34,13 @@
     <!-- Sidebar Menu Links -->
     <ul class="info-menu-list">
       <li><a href="about.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'about.php') echo 'active'; ?>">ABOUT</a></li>
-      <li><a href="news.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'news.php') echo 'active'; ?>">NEWS</a></li>
-      <li><a href="bigideas.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'bigideas.php') echo 'active'; ?>">BIG IDEAS</a></li>
-      <li><a href="awards.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'awards.php') echo 'active'; ?>">AWARDS</a></li>
-      <li><a href="lectures.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'lectures.php') echo 'active'; ?>">LECTURES</a></li>
-      <li><a href="media.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'media.php') echo 'active'; ?>">MEDIA</a></li>
-      <li><a href="shop.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'shop.php') echo 'active'; ?>">SHOP</a></li>
-      <li><a href="jobs.php" class="info-link <//?php if(basename($_SERVER['PHP_SELF']) == 'jobs.php') echo 'active'; ?>">JOBS</a></li>
+      <li><a href="news.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'news.php') echo 'active'; ?>">NEWS</a></li>
+      <li><a href="bigideas.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'bigideas.php') echo 'active'; ?>">BIG IDEAS</a></li>
+      <li><a href="awards.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'awards.php') echo 'active'; ?>">AWARDS</a></li>
+      <li><a href="lectures.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'lectures.php') echo 'active'; ?>">LECTURES</a></li>
+      <li><a href="media.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'media.php') echo 'active'; ?>">MEDIA</a></li>
+      <li><a href="shop.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'shop.php') echo 'active'; ?>">SHOP</a></li>
+      <li><a href="jobs.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'jobs.php') echo 'active'; ?>">JOBS</a></li>
        <li><a href="contact.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'active'; ?>">CONTACT</a></li>
       <li><a href="search.php" class="info-link <?php if(basename($_SERVER['PHP_SELF']) == 'search.php') echo 'active'; ?>">SEARCH</a></li>
     </ul>
